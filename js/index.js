af4c6ca12a19a3a6da24a438415ae8338b3ffe42
patridifusa <p.rufino.a@gmail.com>
2022-10-30 21:36:00 +0000

    Add conditionals in function to exceptions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import strictEquals from './strictEquals.js';
 
-strictEquals();
+strictEquals(1, 1); // True
+strictEquals(NaN, NaN); // False *
+strictEquals(0, -0); // True *
+strictEquals(-0, 0); // True *
+strictEquals(1, '1'); // False
+strictEquals(true, false); // False
+strictEquals(false, false); // True
+strictEquals('water', 'oil'); // False
--- js/strictEquals.js
+++ js/strictEquals.js
@@ -1,5 +1,19 @@
-const strictEquals = () => {
-    console.log('Hello world');
+const strictEquals = (a, b) => {
+    let valueA = a;
+    const valueB = b;
+    // Por defecto, la función Object.is() devuelve TRUE cuando se trata de NaN
+    if (Number.isNaN(a)) {
+        // Para lograr que NaN devuelva FALSE usando la función Object.is(), isNaN devuelve TRUE, lo convierto en string.
+        valueA = a.toString();
+
+        // Por defecto, la función Object.is() devuelve FALSE cuando se trata de comparar 0 positivo y negativo
+    } else if (Number.isInteger(a) && a < 1) {
+        // Para solventarlo, si 'a' es un número y es menor que 1, le doi valor negativo para que siempre sea un negativo con negativo que resulte positivo o que dé a ambos el negativo
+        valueA = -a;
+    }
+
+    const result = Object.is(valueA, valueB);
+    console.log(result);
 };
 
 export default strictEquals;
